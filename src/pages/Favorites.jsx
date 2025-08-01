08368c1dc89156c42249690834ca81974704e927
Damiano Celesti <Damio9422@hotmail.it>
2025-08-01 20:24:24 +0200

    first commit

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+function Favorites() {
+    return (
+        <div className="favorites-empty">
+            <h2>pagina favoriti</h2>
+        </div>
+    )
+}
+
+export default Favorites
---
+++ src/pages/Home.jsx
@@ -0,0 +1,61 @@
+import MovieCard from "../components/MovieCard"
+import { useState, useEffect } from "react"
+import { searchMovies, getPopularMovies } from "../services/api";
+
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+                //console.log(popularMovies)
+            } catch (error) {
+                console.log(error)
+                setError("Failed to load movies")
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+        loadPopularMovies()
+    }, [])
+
+
+    const handleSearch = () => {
+        e.preventDefault()
+        alert(searchQuery)
+    }
+
+    const handleChange = (e) => {
+        setSearchQuery(e.target.value)
+    }
+    return (
+
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                    type="text"
+                    placeholder="Cerca film.."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={handleChange}
+                />
+                <button type="submit" className="search-btn">Search</button>
+            </form>
+            <div className="movie-grid">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
+        </div>
+
+    )
+}
+
+export default Home
